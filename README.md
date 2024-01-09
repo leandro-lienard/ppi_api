f04abe9a78d21a1c532433ee16c084d6b03591e6
leandro-lienard <leandrolienard@gmail.com>
2024-01-09 15:43:23 -0300

    add bonos

---
+++ README.md
@@ -0,0 +1,2 @@
+#Documentacion 
+https://itatppi.github.io/ppi-official-api-docs/api/documentacionPython/
--- ppi_api.py
+++ ppi_api.py
@@ -1,4 +1,3 @@
-##Documentacion https://itatppi.github.io/ppi-official-api-docs/api/documentacionPython/
 
 from ppi_client.api.constants import ACCOUNTDATA_TYPE_ACCOUNT_NOTIFICATION, ACCOUNTDATA_TYPE_PUSH_NOTIFICATION, \
     ACCOUNTDATA_TYPE_ORDER_NOTIFICATION
@@ -26,6 +25,8 @@ MAIN_ACCOUNT = 197663
 CI = "INMEDIATA"
 A_48HS = "A-48HS"
 ppi = {}
+LETRAS = "LETRAS"
+BONOS = "BONOS" 
 
 def login_account():
     Config = configparser.ConfigParser()
@@ -37,19 +38,27 @@ def login_account():
     ppi = PPI(sandbox=False)
     ppi.account.login_api(public_key, private_key)   
 
-def get_cotizacion_letra(letra, liquidacion):
+def get_cotizacion(letra, tipo_instrumento, liquidacion):
     # print("\nSearching Current MarketData")
-    current_market_data = ppi.marketdata.current(letra, "Letras", liquidacion)
+    current_market_data = ppi.marketdata.current(letra, tipo_instrumento, liquidacion)
     # print(current_market_data)
     return current_market_data
     
 def get_ledes_value_usd(ledes_disp, letra, liquidacion):
-    total_value = ledes_disp * get_cotizacion_letra(letra=letra, liquidacion=liquidacion)['price']
+    total_value = ledes_disp * get_cotizacion(letra , LETRAS, liquidacion)['price']
     print("\nfor %s %s the value is U$D %i MEP" % (letra, liquidacion, total_value))
 
 def get_ledes_value_pesos(ledes_disp, letra, liquidacion):
-    total_value = ledes_disp * get_cotizacion_letra(letra=letra, liquidacion=liquidacion)['price']
+    total_value = ledes_disp * get_cotizacion(letra , LETRAS, liquidacion)['price']
     print("\nfor %s %s the value is $ %s pesos" % (letra, liquidacion, format_number(total_value)))
+    
+def get_bonos_value_usd(cant_disp, bono, liquidacion):
+    total_value = cant_disp * get_cotizacion(bono, BONOS, liquidacion)['price']
+    print("\nfor %s %s the value is U$D %i MEP" % (bono, liquidacion, total_value))
+
+def get_bonos_value_pesos(cant_disp, bono, liquidacion):
+    total_value = cant_disp * get_cotizacion(bono, BONOS, liquidacion)['price']
+    print("\nfor %s %s the value is $ %s pesos" % (bono, liquidacion, format_number(total_value)))
     
 def format_number(number):
     return "{:,}".format(round(number))
@@ -61,18 +70,50 @@ def main():
     login_account()
     print("\nBuscando cotizacion LEDES mas conveniente with %i usd ccl" % POZO_A_SIMULAR)
     
-    ccl = get_cotizacion_letra(letra = 'XE4C', liquidacion=CI)
-    mep_ci = get_cotizacion_letra(letra = 'XE4D', liquidacion=CI)
-    mep_48hs = get_cotizacion_letra(letra = 'XE4D', liquidacion=A_48HS)
-    pesos_ci = get_cotizacion_letra(letra = 'X18E4', liquidacion=CI)
+    ccl = get_cotizacion('XE4C',LETRAS, CI)
+    mep_ci = get_cotizacion( 'XE4D', LETRAS, CI)
+    mep_48hs = get_cotizacion( 'XE4D', LETRAS, A_48HS)
+    pesos_ci = get_cotizacion( 'X18E4', LETRAS, CI)
     print("\nccl ci: %s \nmep ci %s \nmep 48hs %s \npesos ci %s" % (ccl, mep_ci, mep_48hs, pesos_ci))
     
     LEDES_disp = round( POZO_A_SIMULAR / float(ccl['price']))
     
     print("\nLEDES DISPONIBLES ", LEDES_disp)
-    get_ledes_value_usd(ledes_disp=LEDES_disp, letra='XE4D', liquidacion=CI)
-    get_ledes_value_usd(ledes_disp=LEDES_disp, letra='XE4D', liquidacion=A_48HS)
-    get_ledes_value_pesos(ledes_disp=LEDES_disp, letra='X18E4', liquidacion=CI)
+    get_ledes_value_usd(LEDES_disp, 'XE4D', CI)
+    get_ledes_value_usd(LEDES_disp, 'XE4D', A_48HS)
+    get_ledes_value_pesos(LEDES_disp, 'X18E4', CI)
+    
+    #************************* BONOS AL 30 *****************************************
+    ccl = get_cotizacion('AL30C', BONOS, CI)
+    mep_ci = get_cotizacion('AL30D', BONOS, CI)
+    mep_48hs = get_cotizacion('AL30D', BONOS, A_48HS)
+    pesos_ci = get_cotizacion('AL30', BONOS, CI)
+
+    print("\nAL30")    
+    print("\nccl ci: %s \nmep ci %s \nmep 48hs %s \npesos ci %s" % (ccl, mep_ci, mep_48hs, pesos_ci))
+    
+    BONOS_DISP = round( POZO_A_SIMULAR / float(ccl['price']))
+    
+    print("\nAL30 DISPONIBLES ", BONOS_DISP) #precio cada 100 units?
+    get_bonos_value_usd(BONOS_DISP, 'AL30D', CI)
+    get_bonos_value_usd(BONOS_DISP, 'AL30D', A_48HS)
+    get_bonos_value_pesos(BONOS_DISP, 'AL30', CI)
+    
+    #************************* BONOS GD 30 *****************************************
+    ccl = get_cotizacion('GD30C', BONOS, CI)
+    mep_ci = get_cotizacion('GD30D', BONOS, CI)
+    mep_48hs = get_cotizacion('GD30D', BONOS, A_48HS)
+    pesos_ci = get_cotizacion('GD30', BONOS, CI)
+
+    print("\nGD30")    
+    print("\nccl ci: %s \nmep ci %s \nmep 48hs %s \npesos ci %s" % (ccl, mep_ci, mep_48hs, pesos_ci))
+    
+    BONOS_DISP = round( POZO_A_SIMULAR / float(ccl['price']))
+    
+    print("\nGD30 DISPONIBLES ", BONOS_DISP) #precio cada 100 units?
+    get_bonos_value_usd(BONOS_DISP, 'GD30D', CI)
+    get_bonos_value_usd(BONOS_DISP, 'GD30D', A_48HS)
+    get_bonos_value_pesos(BONOS_DISP, 'GD30', CI)
     
 if __name__ == "__main__":
     main()
